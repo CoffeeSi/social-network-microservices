3eb3bf61d3b8bb3372a6c3f644b12772f0335034
Denis <denislee56@gmail.com>
2026-05-12 02:21:02 +0500

    feat: fix repo, migrate to mongov2

--- user-service/internal/repository/db/mongo.go
+++ user-service/internal/repository/db/mongo.go
@@ -3,14 +3,14 @@ package db
 import (
 	"context"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func InitMongoDB(uri string) (*mongo.Client, error) {
 
 	clientOpt := options.Client().ApplyURI(uri)
-	client, err := mongo.Connect(context.Background(), clientOpt)
+	client, err := mongo.Connect(clientOpt)
 	if err != nil {
 		return nil, err
 	}
