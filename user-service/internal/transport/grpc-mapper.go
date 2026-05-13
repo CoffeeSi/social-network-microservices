fe5023ba4436df94b217f324267e0c4c6450ad32
Denis <denislee56@gmail.com>
2026-05-14 03:22:15 +0500

    feat: 3 methods in handler

---
+++ user-service/internal/transport/grpc-mapper.go
@@ -0,0 +1,18 @@
+package transport
+
+import (
+	"github.com/CoffeeSi/social-network-microservices/user-service/internal/model"
+	pb "github.com/CoffeeSi/social-network-microservices/user-service/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func userToProto(user *model.User) *pb.User {
+	return &pb.User{
+		Id:        user.ID,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		Dob:       timestamppb.New(user.DOB),
+		CreatedAt: timestamppb.New(user.CreatedAt),
+	}
+}
--- user-service/internal/usecase/user-usecase.go
+++ user-service/internal/usecase/user-usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 	"errors"
+	"strings"
 	"time"
 
 	"github.com/CoffeeSi/social-network-microservices/user-service/internal/model"
@@ -13,8 +14,10 @@ type UserRepo interface {
 	CreateUser(ctx context.Context, user model.User) (model.User, error)
 	GetUsers(ctx context.Context, pageSize, page int32) ([]model.User, int32, error)
 	GetUser(ctx context.Context, id string) (model.User, error)
-	PatchUser(ctx context.Context, id string, updateData model.User) error
+	PatchUser(ctx context.Context, id string, updateData model.User) (model.User, error)
 	GetUserByEmail(ctx context.Context, email string) (model.User, error)
+	DeleteUser(ctx context.Context, id string) error
+	ChangePassword(ctx context.Context, id, password string) error
 }
 
 type UserUseCase struct {
@@ -57,4 +60,78 @@ func (us *UserUseCase) GetUserByID(ctx context.Context, id string) (model.User, 
 		return model.User{}, err
 	}
 	return res, nil
+}
+
+func (us *UserUseCase) GetUsers(ctx context.Context, pageSize, page int32) ([]model.User, int32, error) {
+	if pageSize <= 0 {
+		pageSize = 20
+	}
+	if pageSize > 100 {
+		pageSize = 100
+	}
+	if page <= 0 {
+		page = 1
+	}
+	res, total, err := us.repo.GetUsers(ctx, pageSize, page)
+	if err != nil {
+		return nil, 0, err
+	}
+	return res, total, nil
+}
+
+func (us *UserUseCase) PatchUser(ctx context.Context, dto dto.PatchUserDTO) (model.User, error) {
+	//email check
+	if !model.EmailRegex.MatchString(dto.Email) && dto.Email != "" {
+		return model.User{}, errors.New("email is bad")
+	}
+	//age check?
+	minAge := time.Now().AddDate(-13, 0, 0)
+	if !dto.DOB.IsZero() && dto.DOB.After(minAge) {
+		return model.User{}, errors.New("user must be at least 13 years old")
+	}
+	userModel := model.User{
+		FirstName: dto.FirstName,
+		LastName:  dto.LastName,
+		Email:     dto.Email,
+		DOB:       dto.DOB,
+	}
+	res, err := us.repo.PatchUser(ctx, dto.ID, userModel)
+	if err != nil {
+		return model.User{}, err
+	}
+	return res, nil
+}
+
+func (us *UserUseCase) GetUserByEmail(ctx context.Context, email string) (model.User, error) {
+	email = strings.TrimSpace(strings.ToLower(email))
+	if email == "" || !model.EmailRegex.MatchString(email) {
+		return model.User{}, errors.New("email is empty or bad")
+	}
+	res, err := us.repo.GetUserByEmail(ctx, email)
+	if err != nil {
+		return model.User{}, err
+	}
+	return res, nil
+}
+func (us *UserUseCase) DeleteUser(ctx context.Context, id string) error {
+	if id == "" {
+		return errors.New("Id is empty")
+	}
+	err := us.repo.DeleteUser(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (us *UserUseCase) ChangePassword(ctx context.Context, id, password string) error {
+	if id == "" {
+		return errors.New("Id is empty")
+	}
+	err := us.repo.ChangePassword(ctx, id, password)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
