801c29fa039ee325805f1e0bcd344af551baf23b
IsFariza <fariza.ismag@gmail.com>
2026-05-14 22:39:00 +0500

    feat(content): add handler

---
+++ content-service/internal/transport/content-mapper.go
@@ -0,0 +1,34 @@
+package transport
+
+import (
+	"github.com/CoffeeSi/social-network-microservices/content-service/internal/model"
+	pb "github.com/CoffeeSi/social-network-microservices/content-service/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func postToProto(p *model.Post) *pb.Post {
+	if p == nil {
+		return nil
+	}
+	return &pb.Post{
+		Id:           p.ID,
+		AuthorId:     p.AuthorID,
+		Content:      p.Content,
+		MediaUrls:    p.MediaURLs,
+		LikeCount:    p.LikeCount,
+		CommentCount: p.CommentCount,
+		CreatedAt:    timestamppb.New(p.CreatedAt),
+	}
+}
+func commentToProto(c *model.Comment) *pb.Comment {
+	if c == nil {
+		return nil
+	}
+	return &pb.Comment{
+		Id:        c.ID,
+		PostId:    c.PostID,
+		UserId:    c.UserID,
+		Text:      c.Text,
+		CreatedAt: timestamppb.New(c.CreatedAt),
+	}
+}
