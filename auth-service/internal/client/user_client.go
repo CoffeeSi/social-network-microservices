bd8d6c57e1cb9a468c266763d3093c2f9d5d344d
Yevgeniy Averyanov <ewgenik02032006@gmail.com>
2026-05-14 16:49:40 +0500

    feat: scaffold auth-service with gRPC user proto definitions, validation utilities, and core business logic layers

---
+++ auth-service/internal/client/user_client.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"context"
+
+	"github.com/CoffeeSi/social-network-microservices/auth-service/internal/model"
+	pb_user "github.com/CoffeeSi/social-network-microservices/auth-service/proto/user"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type UserClient struct {
+	pbUserClient pb_user.UserServiceClient
+}
+
+func NewUserClient(grpcConn *grpc.ClientConn) *UserClient {
+	return &UserClient{
+		pbUserClient: pb_user.NewUserServiceClient(grpcConn),
+	}
+}
+
+func (c *UserClient) CreateUser(ctx context.Context, req model.Auth) error {
+	user := pb_user.CreateUserRequest{
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		Dob:       timestamppb.New(req.DOB),
+		Email:     req.Email,
+		Password:  req.Password,
+	}
+	_, err := c.pbUserClient.CreateUser(ctx, &user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *UserClient) GetUserByEmail(ctx context.Context, email string) (model.Auth, error) {
+	pbReq := &pb_user.GetUserByEmailRequest{
+		Email: email,
+	}
+
+	user, err := c.pbUserClient.GetUserByEmail(ctx, pbReq)
+	if err != nil {
+		return model.Auth{}, err
+	}
+	return model.Auth{
+		ID:        user.Id,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		DOB:       user.Dob.AsTime(),
+		Email:     user.Email,
+		Password:  user.Password,
+	}, nil
+}
